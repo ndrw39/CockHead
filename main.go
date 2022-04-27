1749f14d84109266ef95b092ffbdcf17924e4a1b
ndrw39 <andrey.oshnik@gmail.com>
2022-04-27 16:27:32 +0200

    Test message

--- main.go
+++ main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
-	"cockHead/conf"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	//"net/http"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+const (
+	ApiToken string = "5342559232:AAEB9q73QHm9EaNVoZMa3lS2o0iwL2fg9Po"
+	ApiUrl   string = "https://api.telegram.org/bot5342559232:AAEB9q73QHm9EaNVoZMa3lS2o0iwL2fg9Po"
 )
 
 func main() {
-	fmt.Print(conf.API_TOKEN)
 	router := gin.Default()
 	router.TrustedPlatform = gin.PlatformGoogleAppEngine
 	router.TrustedPlatform = "X-CDN-IP"
 
-	router.POST("/"+conf.API_TOKEN, func(c *gin.Context) {
+	router.POST("/"+ApiToken, func(c *gin.Context) {
 		// If you set TrustedPlatform, ClientIP() will resolve the
 		// corresponding header and return IP directly
-		fmt.Printf("ClientIP: %s\n", c.ClientIP())
+		sendMessage(834117686, "Hello Dev")
 	})
 
 	err := router.Run()
 	if err != nil {
 		return
 	}
+}
+
+func sendMessage(userId int, message string) {
+	body := url.Values{
+		"chat_id": {string(userId)},
+		"text":    {message},
+	}
+
+	resp, err := http.PostForm(ApiUrl+"/sendMessage", body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
 }
