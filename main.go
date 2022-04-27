8a6ecca1edb89a38d90e67c599543fcd75a8c59c
ndrw39 <andrey.oshnik@gmail.com>
2022-04-27 14:05:54 +0200

    Method POST from WebHook

--- main.go
+++ main.go
@@ -3,28 +3,24 @@ package main
 import (
 	"cockHead/conf"
 	"fmt"
-	//"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 	//"net/http"
 )
 
 func main() {
 	fmt.Print(conf.API_TOKEN)
-	//router := gin.Default()
-	//router.TrustedPlatform = gin.PlatformGoogleAppEngine
-	//// Or set your own trusted request header for another trusted proxy service
-	//// Don't set it to any suspect request header, it's unsafe
-	//router.TrustedPlatform = "X-CDN-IP"
-	//
-	//router.GET("/", func(c *gin.Context) {
-	//	// If you set TrustedPlatform, ClientIP() will resolve the
-	//	// corresponding header and return IP directly
-	//	fmt.Printf("ClientIP: %s\n", c.ClientIP())
-	//})
-	//router.GET("/albums", getAlbums)
-	//router.POST("/albums", postAlbums)
-	//
-	//err := router.Run()
-	//if err != nil {
-	//	return
-	//}
+	router := gin.Default()
+	router.TrustedPlatform = gin.PlatformGoogleAppEngine
+	router.TrustedPlatform = "X-CDN-IP"
+
+	router.POST("/"+conf.API_TOKEN, func(c *gin.Context) {
+		// If you set TrustedPlatform, ClientIP() will resolve the
+		// corresponding header and return IP directly
+		fmt.Printf("ClientIP: %s\n", c.ClientIP())
+	})
+
+	err := router.Run()
+	if err != nil {
+		return
+	}
 }
